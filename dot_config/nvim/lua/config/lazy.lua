9d7d2c6d4c61e0c1c48eae48c6087608377d002e
Sandro <me@sandrog.dev>
2024-11-01 20:52:55 +0100

    init

---
+++ dot_config/nvim/lua/config/lazy.lua
@@ -0,0 +1,108 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    -- add LazyVim and import its plugins
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import/override with your plugins
+    { import = "plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  install = { colorscheme = { "tokyonight", "habamax" } },
+  checker = {
+    enabled = true, -- check for plugin updates periodically
+    notify = false, -- notify on update
+  }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+local set = vim.opt_local
+
+-- Set local settings for terminal buffers
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", {}),
+  callback = function()
+    set.number = false
+    set.relativenumber = false
+    set.scrolloff = 0
+
+    vim.bo.filetype = "terminal"
+  end,
+})
+
+-- Easily hit escape in terminal mode.
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+-- Open a terminal at the bottom of the screen with a fixed height.
+vim.keymap.set("n", ",st", function()
+  vim.cmd.new()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 12)
+  vim.wo.winfixheight = true
+  vim.cmd.term()
+end)
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  command = "Copilot disable",
+})
+
+-- -- Global variable to keep track of the toggle state
+-- _G.copilot_suggestions_enabled = true
+--
+-- -- Function to toggle Copilot suggestions
+-- function _G.toggle_copilot_suggestions()
+--   if _G.copilot_suggestions_enabled then
+--     -- Disable Copilot suggestions
+--     vim.api.nvim_create_autocmd("BufEnter", {
+--       pattern = "*",
+--       callback = function()
+--         require("copilot.client").buf_detach()
+--       end,
+--     })
+--     print("Copilot suggestions disabled")
+--   else
+--     -- Enable Copilot suggestions
+--     vim.api.nvim_clear_autocmds({ event = "BufEnter" })
+--     print("Copilot suggestions enabled")
+--   end
+--   _G.copilot_suggestions_enabled = not _G.copilot_suggestions_enabled
+-- end
+--
+-- -- Bind the toggle function to a Neovim command
+-- vim.api.nvim_create_user_command("ToggleCopilot", _G.toggle_copilot_suggestions, {})
---
+++ dot_config/nvim/lua/plugins/core.lua
@@ -0,0 +1,92 @@
+return {
+  -- lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+    },
+  },
+  {
+    "sandro-sh/oldworld.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oldworld",
+    },
+  },
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   config = function()
+  --     require("supermaven-nvim").setup({})
+  --   end,
+  -- },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     "supermaven-inc/supermaven-nvim",
+  --     -- build = ":SupermavenUseFree", -- remove this line if you are using pro
+  --     opts = {},
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, 1, {
+  --       name = "supermaven",
+  --       group_index = 1,
+  --       priority = 100,
+  --     })
+  --   end,
+  -- },
+  {
+    "folke/zen-mode.nvim",
+    opts = {},
+  },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require("oil").setup({
+        columns = {
+          "icon",
+          -- "permissions",
+          -- "size",
+          -- "mtime",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      })
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        auto_insert_mode = true,
+        show_help = true,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot ",
+        window = {
+          width = 0.4,
+        },
+        selection = function(source)
+          local select = require("CopilotChat.select")
+          return select.visual(source) or select.buffer(source)
+        end,
+        model = "claude-3.5-sonnet",
+      }
+    end,
+  },
+}

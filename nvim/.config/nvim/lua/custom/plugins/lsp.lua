2862c81c11bf83a365e800e1c40033da0228cd9c
Sandro <me@sandrog.dev>
2025-03-13 22:23:44 +0100

    fix: typescript lsp

--- nvim/.config/nvim/lua/custom/plugins/lsp.lua
+++ nvim/.config/nvim/lua/custom/plugins/lsp.lua
@@ -62,95 +62,108 @@ return {
           },
         },
         marksman = {},
-        -- vtsls = {
-        --   filetypes = {
-        --     "javascript",
-        --     "javascriptreact",
-        --     "javascript.jsx",
-        --     "typescript",
-        --     "typescriptreact",
-        --     "typescript.tsx",
-        --   },
-        --   settings = {
-        --     complete_function_calls = true,
-        --     vtsls = {
-        --       enableMoveToFileCodeAction = true,
-        --       autoUseWorkspaceTsdk = true,
-        --       experimental = {
-        --         maxInlayHintLength = 30,
-        --         completion = {
-        --           enableServerSideFuzzyMatch = true,
-        --         },
-        --       },
-        --     },
-        --     typescript = {
-        --       updateImportsOnFileMove = { enabled = "always" },
-        --       suggest = {
-        --         completeFunctionCalls = true,
-        --       },
-        --       inlayHints = {
-        --         enumMemberValues = { enabled = true },
-        --         functionLikeReturnTypes = { enabled = true },
-        --         parameterNames = { enabled = "literals" },
-        --         parameterTypes = { enabled = true },
-        --         propertyDeclarationTypes = { enabled = true },
-        --         variableTypes = { enabled = false },
-        --       },
-        --     },
-        --   },
-        --   keys = {
-        --     {
-        --       "gD",
-        --       function()
-        --         local params = vim.lsp.util.make_position_params()
-        --         vim.lsp.execute({
-        --           command = "typescript.goToSourceDefinition",
-        --           arguments = { params.textDocument.uri, params.position },
-        --           open = true,
-        --         })
-        --       end,
-        --       desc = "Goto Source Definition",
-        --     },
-        --     {
-        --       "gR",
-        --       function()
-        --         vim.lsp.execute({
-        --           command = "typescript.findAllFileReferences",
-        --           arguments = { vim.uri_from_bufnr(0) },
-        --           open = true,
-        --         })
-        --       end,
-        --       desc = "File References",
-        --     },
-        --     {
-        --       "<leader>co",
-        --       vim.lsp.action["source.organizeImports"],
-        --       desc = "Organize Imports",
-        --     },
-        --     {
-        --       "<leader>cM",
-        --       vim.lsp.action["source.addMissingImports.ts"],
-        --       desc = "Add missing imports",
-        --     },
-        --     {
-        --       "<leader>cu",
-        --       vim.lsp.action["source.removeUnused.ts"],
-        --       desc = "Remove unused imports",
-        --     },
-        --     {
-        --       "<leader>cD",
-        --       vim.lsp.action["source.fixAll.ts"],
-        --       desc = "Fix all diagnostics",
-        --     },
-        --     {
-        --       "<leader>cV",
-        --       function()
-        --         vim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
-        --       end,
-        --       desc = "Select TS workspace version",
-        --     },
-        --   },
-        -- },
+        vtsls = {
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+          },
+          init_options = {
+            preferences = {
+              importModuleSpecifierPreference = "non-relative",
+            },
+          },
+          settings = {
+            complete_function_calls = true,
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
+            },
+          },
+          keys = {
+            {
+              "gD",
+              function()
+                local params = vim.lsp.util.make_position_params()
+                vim.lsp.execute({
+                  command = "typescript.goToSourceDefinition",
+                  arguments = { params.textDocument.uri, params.position },
+                  open = true,
+                })
+              end,
+              desc = "Goto Source Definition",
+            },
+            {
+              "gR",
+              function()
+                vim.lsp.execute({
+                  command = "typescript.findAllFileReferences",
+                  arguments = { vim.uri_from_bufnr(0) },
+                  open = true,
+                })
+              end,
+              desc = "File References",
+            },
+            {
+              "<leader>co",
+              function()
+                vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } } })
+              end,
+              desc = "Organize Imports",
+            },
+            {
+              "<leader>cM",
+              function()
+                vim.lsp.buf.code_action({ context = { only = { "source.addMissingImports" } } })
+              end,
+              desc = "Add missing imports",
+            },
+            {
+              "<leader>cu",
+              function()
+                vim.lsp.buf.code_action({ context = { only = { "source.removeUnused" } } })
+              end,
+              desc = "Remove unused imports",
+            },
+            {
+              "<leader>cD",
+              function()
+                vim.lsp.buf.code_action({ context = { only = { "source.fixAll" } } })
+              end,
+              desc = "Fix all diagnostics",
+            },
+            {
+              "<leader>cV",
+              function()
+                vim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+              end,
+              desc = "Select TS workspace version",
+            },
+          },
+        },
       },
       ---@type table<string, fun(server:string, opts:lspconfig.options):boolean?>
       setup = {

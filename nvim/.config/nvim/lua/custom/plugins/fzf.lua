39be80585eaf769dfcbc8b07af9d3cdc8a37a8ae
Sandro <me@sandrog.dev>
2025-03-24 23:45:38 +0100

    add gitignore action to live grep

--- nvim/.config/nvim/lua/custom/plugins/fzf.lua
+++ nvim/.config/nvim/lua/custom/plugins/fzf.lua
@@ -4,11 +4,21 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       local root = require 'custom.root'
+      local actions = require('fzf-lua').actions
 
       require('fzf-lua').setup {
         hls = { border = 'FloatBorder' },
         winopts = {
           border = 'single',
+        },
+        grep = {
+          actions = {
+            -- actions inherit from 'actions.files' and merge
+            -- this action toggles between 'grep' and 'live_grep'
+            ['ctrl-g'] = { actions.grep_lgrep },
+            -- uncomment to enable '.gitignore' toggle for grep
+            ['ctrl-r'] = { actions.toggle_ignore },
+          },
         },
       }
 
@@ -22,25 +32,24 @@ return {
       vim.keymap.set('n', '<leader>/', function()
         require('fzf-lua').live_grep { cwd = root.get() }
       end, { desc = 'FzF Live Grep' })
-      vim.keymap.set('n', '<leader>fg', function()
-        require('fzf-lua').grep { cwd = root.get() }
-      end, { desc = '[G]rep' })
       vim.keymap.set('n', '<leader>fb', require('fzf-lua').buffers, { desc = '[B]uffer' })
       vim.keymap.set('n', '<leader>fm', require('fzf-lua').manpages, { desc = '[M]anpages' })
       vim.keymap.set('n', '<leader>fC', require('fzf-lua').colorschemes, { desc = '[C]olorschemes' })
       vim.keymap.set('n', '<leader>fa', require('fzf-lua').awesome_colorschemes, { desc = '[A]wesome Colorschemes' })
-
-      vim.keymap.set('n', '<leader>fd', function()
-        require('fzf-lua').files {
-          cwd = vim.fs.joinpath(vim.fn.stdpath 'data'),
-        }
-      end, { desc = '[D]ata' })
-
       vim.keymap.set('n', '<leader>fc', function()
         require('fzf-lua').files {
           cwd = vim.fn.stdpath 'config',
         }
       end, { desc = '[C]onfig' })
+
+      -- vim.keymap.set('n', '<leader>fg', function()
+      --   require('fzf-lua').grep { cwd = root.get() }
+      -- end, { desc = '[G]rep' })
+      -- vim.keymap.set('n', '<leader>fd', function()
+      --   require('fzf-lua').files {
+      --     cwd = vim.fs.joinpath(vim.fn.stdpath 'data'),
+      --   }
+      -- end, { desc = '[D]ata' })
     end,
   },
 }

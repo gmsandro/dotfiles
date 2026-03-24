d7b0d744e89383f2dbc80f2cf89471e632b29e7e
Sandro G <sandro@vgh.ovh>
2026-03-24 18:39:17 +0100

    update nvim-treesitter to use new main branch

--- nvim/.config/nvim/lua/custom/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,64 +1,130 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
     config = function()
-      local configs = require 'nvim-treesitter.configs'
-
-      configs.setup {
-        ensure_installed = {
-          'vimdoc',
-          'c',
-          'lua',
-          'rust',
-          'bash',
-          'markdown',
-          'go',
-          'bash',
-          'luadoc',
-          'printf',
-          'tsx',
-          'javascript',
-          'typescript',
-          'html',
-          'jsdoc',
-        },
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-
-        -- Add textobjects configuration
-        textobjects = {
-          move = {
-            enable = true,
-            goto_next_start = { [']f'] = '@function.outer', [']c'] = '@class.outer', [']a'] = '@parameter.inner' },
-            goto_next_end = { [']F'] = '@function.outer', [']C'] = '@class.outer', [']A'] = '@parameter.inner' },
-            goto_previous_start = { ['[f'] = '@function.outer', ['[c'] = '@class.outer', ['[a'] = '@parameter.inner' },
-            goto_previous_end = { ['[F'] = '@function.outer', ['[C'] = '@class.outer', ['[A'] = '@parameter.inner' },
-          },
-        },
+      require('nvim-treesitter').setup {
+        install_dir = vim.fn.stdpath('data') .. '/site',
       }
 
-      local treesitter_parser_config = require('nvim-treesitter.parsers').get_parser_configs()
-      treesitter_parser_config.templ = {
-        install_info = {
-          url = 'https://github.com/vrischmann/tree-sitter-templ.git',
-          files = { 'src/parser.c', 'src/scanner.c' },
-          branch = 'master',
-        },
+      require('nvim-treesitter').install {
+        'vimdoc',
+        'c',
+        'lua',
+        'rust',
+        'bash',
+        'markdown',
+        'go',
+        'luadoc',
+        'printf',
+        'tsx',
+        'javascript',
+        'typescript',
+        'html',
+        'jsdoc',
       }
 
-      vim.treesitter.language.register('templ', 'templ')
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          if vim.bo[args.buf].buftype ~= "" then
+            return
+          end
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+
+      -- vim.api.nvim_create_autocmd('FileType', {
+      --   pattern = {
+      --     'c',
+      --     'go',
+      --     'html',
+      --     'javascript',
+      --     'lua',
+      --     'markdown',
+      --     'java',
+      --     'rust',
+      --     'tsx',
+      --     'typescript',
+      --     'vim',
+      --   },
+      --   callback = function()
+      --     vim.treesitter.start()
+      --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      --   end,
+      -- })
     end,
   },
+
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+      require('nvim-treesitter-textobjects').setup {
+        move = {
+          set_jumps = true,
+        },
+      }
+
+      local move = require 'nvim-treesitter-textobjects.move'
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
+        move.goto_next_start('@function.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']c', function()
+        move.goto_next_start('@class.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']a', function()
+        move.goto_next_start('@parameter.inner', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']F', function()
+        move.goto_next_end('@function.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']C', function()
+        move.goto_next_end('@class.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, ']A', function()
+        move.goto_next_end('@parameter.inner', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[f', function()
+        move.goto_previous_start('@function.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[c', function()
+        move.goto_previous_start('@class.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[a', function()
+        move.goto_previous_start('@parameter.inner', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[F', function()
+        move.goto_previous_end('@function.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[C', function()
+        move.goto_previous_end('@class.outer', 'textobjects')
+      end)
+
+      vim.keymap.set({ 'n', 'x', 'o' }, '[A', function()
+        move.goto_previous_end('@parameter.inner', 'textobjects')
+      end)
+    end,
+  },
+
   {
     'windwp/nvim-ts-autotag',
-    event = 'BufReadPre',
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       require('nvim-ts-autotag').setup()
     end,
